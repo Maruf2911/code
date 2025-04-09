f19492228116eec91a17c72e05fad70a2f10d484
Maruf2911 <maruf5588103@gmail.com>
2025-04-09 11:18:42 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+      },
+    ],
+  },
+  experimental: {
+    mdxRs: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
